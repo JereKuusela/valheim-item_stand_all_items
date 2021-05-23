7e63ebb04bdb2f7d2124728513c2a4489031123d
Jere Kuusela <jere.kuusela@iki.fi>
2021-05-23 10:05:43 +0300

    Initial commit

---
+++ ItemStandAllItems/ItemStandAllItems.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using HarmonyLib;
+
+namespace ItemStandAllItems
+{
+  [BepInPlugin("valheim.jere.item-stand-all-items", "ItemStandAllItems", "1.0.0.0")]
+  public class ItemStandAllItems : BaseUnityPlugin
+  {
+    void Awake()
+    {
+      var harmony = new Harmony("valheim.jere.item-stand-all-items");
+      harmony.PatchAll();
+    }
+  }
+
+  [HarmonyPatch(typeof(ItemStand), "CanAttach", new Type[] { typeof(ItemDrop.ItemData) })]
+  public class ItemStand_CanAttach
+  {
+    public static bool Prefix(ItemDrop.ItemData item, ref bool __result, ItemStand __instance)
+    {
+      if (__instance.m_name == "$piece_itemstand") {
+        __result = true;
+        return false;
+      }
+      return true;
+    }
+  }
+}
