d8af16249040181a6ef607cefdc5b2c20775090b
Jere Kuusela <jere.kuusela@iki.fi>
2021-09-19 08:57:39 +0300

    Code style changes.

--- ItemStandAllItems/ItemStandAllItems.cs
+++ ItemStandAllItems/ItemStandAllItems.cs
@@ -4,18 +4,15 @@ using BepInEx.Configuration;
 using UnityEngine;
 using HarmonyLib;
 
-namespace ItemStandAllItems
-{
+namespace ItemStandAllItems {
   [BepInPlugin("valheim.jere.item_stand_all_items", "ItemStandAllItems", "1.3.0.0")]
-  public class ItemStandAllItems : BaseUnityPlugin
-  {
+  public class ItemStandAllItems : BaseUnityPlugin {
     public static ConfigEntry<bool> configUseLegacyAttaching;
     public static ConfigEntry<bool> configPreventItemsWithItemDrop;
     public static ConfigEntry<bool> configRemoveRootOffset;
     public static ConfigEntry<bool> configRemoveRootRotation;
     public static ConfigEntry<bool> configPreferRootObject;
-    void Awake()
-    {
+    public void Awake() {
       configUseLegacyAttaching = Config.Bind("General", "UseLegacyAttaching", false, "Use the previous attach way on version 1.1.0 (works for less items)");
       configPreventItemsWithItemDrop = Config.Bind("General", "PreventItemDrops", false, "Enable for less instrusive approach (works for less items).");
       configRemoveRootOffset = Config.Bind("General", "RemoveRootOffset", true, "Some items (like Carrot seeds) have a weird offset at the root level.");
@@ -25,103 +22,102 @@ namespace ItemStandAllItems
       harmony.PatchAll();
     }
 
-    private static GameObject GetAttachObject(GameObject item)
-    {
-      if (configUseLegacyAttaching.Value)
-      {
+    private static GameObject GetAttachObject(GameObject item) {
+      if (configUseLegacyAttaching.Value) {
         // Legacy way only finds the object with a collider.
         // May not contain all models of the item resulting only in a partial item (like Graydward eye will miss the eye).
         var collider = item.transform.GetComponentInChildren<Collider>();
-        if (collider) return collider.transform.gameObject;
-      }
-      else
-      {
-        if (!configPreferRootObject.Value)
-        {
-          var child = GetRenderingChild(item);
-          if (child != item) return child;
+        if (collider) {
+          return collider.transform.gameObject;
         }
-        if (configRemoveRootOffset.Value)
+      } else {
+        if (!configPreferRootObject.Value) {
+          var child = GetRenderingChild(item);
+          if (child != item) {
+            return child;
+          }
+        }
+        if (configRemoveRootOffset.Value) {
           item.transform.localPosition = Vector3.zero;
-        if (configRemoveRootRotation.Value)
+        }
+
+        if (configRemoveRootRotation.Value) {
           item.transform.localRotation = Quaternion.identity;
+        }
         // New way always returns the whole item. This means extra components must be removed.
         return item;
       }
       return null;
     }
     // Copypaste from base game code.
-    private static GameObject GetTransform(GameObject item, string name)
-    {
-      Transform transform = item.transform.Find(name);
-      if (transform) return transform.gameObject;
-      return null;
+    private static GameObject GetTransform(GameObject item, string name) {
+      var transform = item.transform.Find(name);
+      return transform ? transform.gameObject : null;
     }
     // In most cases the child object is better since it doesn't contain bad transform or ItemDrop component.
     // However this won't work if there are multiple children (like with Carrot seeds).
-    private static GameObject GetRenderingChild(GameObject item)
-    {
-      if (item.GetComponent<MeshRenderer>() || item.transform.childCount != 1) return item;
-      return item.transform.GetChild(0).gameObject;
+    private static GameObject GetRenderingChild(GameObject item) {
+      return item.GetComponent<MeshRenderer>() || item.transform.childCount != 1 ? item : item.transform.GetChild(0).gameObject;
     }
-    public static GameObject GetAttach(GameObject item)
-    {
+    public static GameObject GetAttach(GameObject item) {
       // Base game also uses "attach" transform but explicitly disabled for some items.
       // Check it first as it's the safest pick.
       var obj = GetTransform(item, "attach");
-      if (obj) return obj;
+      if (obj) {
+        return obj;
+      }
+
       obj = GetAttachObject(item);
-      if (obj && configPreventItemsWithItemDrop.Value && obj.GetComponent<ItemDrop>()) return null;
-      return obj;
+      return obj && configPreventItemsWithItemDrop.Value && obj.GetComponent<ItemDrop>() ? null : obj;
     }
   }
 
   // Adds additional check with the custom attach code.
   [HarmonyPatch(typeof(ItemStand), "CanAttach")]
-  public class ItemStand_CanAttach
-  {
-    public static void Postfix(ItemStand __instance, ItemDrop.ItemData item, ref bool __result)
-    {
-      if (!__result && __instance.m_name == "$piece_itemstand") __result = ItemStandAllItems.GetAttach(item.m_dropPrefab) != null;
+  public class ItemStand_CanAttach {
+    public static void Postfix(ItemStand __instance, ItemDrop.ItemData item, ref bool __result) {
+      if (!__result && __instance.m_name == "$piece_itemstand") {
+        __result = ItemStandAllItems.GetAttach(item.m_dropPrefab) != null;
+      }
     }
   }
 
 
   // Adds additional check with the custom attach code.
   [HarmonyPatch(typeof(ItemStand), "GetAttachPrefab")]
-  public class ItemStand_GetAttachPrefab
-  {
-    public static void Postfix(GameObject item, ref GameObject __result)
-    {
-      if (__result == null) __result = ItemStandAllItems.GetAttach(item);
+  public class ItemStand_GetAttachPrefab {
+    public static void Postfix(GameObject item, ref GameObject __result) {
+      if (__result == null) {
+        __result = ItemStandAllItems.GetAttach(item);
+      }
     }
   }
 
   [HarmonyPatch(typeof(ItemStand), "SetVisualItem")]
-  public class ItemStand_SetVisualItem
-  {
-    public static Dictionary<ZDO, ZNetView> m_instances(ZNetScene obj) => Traverse.Create(obj).Field<Dictionary<ZDO, ZNetView>>("m_instances").Value;
-    public static void Postfix(GameObject ___m_visualItem)
-    {
-      if (___m_visualItem && !ItemStandAllItems.configPreventItemsWithItemDrop.Value)
-      {
-        UnityEngine.Object.Destroy(___m_visualItem.GetComponent<ItemDrop>());
-        UnityEngine.Object.Destroy(___m_visualItem.GetComponent<Rigidbody>());
-        UnityEngine.Object.Destroy(___m_visualItem.GetComponent<ZSyncTransform>());
-        UnityEngine.Object.Destroy(___m_visualItem.GetComponent<ParticleSystem>());
+  public class ItemStand_SetVisualItem {
+    public static Dictionary<ZDO, ZNetView> Instances(ZNetScene obj) {
+      return Traverse.Create(obj).Field<Dictionary<ZDO, ZNetView>>("m_instances").Value;
+    }
+
+    public static void Postfix(GameObject ___m_visualItem) {
+      if (___m_visualItem && !ItemStandAllItems.configPreventItemsWithItemDrop.Value) {
+        Object.Destroy(___m_visualItem.GetComponent<ItemDrop>());
+        Object.Destroy(___m_visualItem.GetComponent<Rigidbody>());
+        Object.Destroy(___m_visualItem.GetComponent<ZSyncTransform>());
+        Object.Destroy(___m_visualItem.GetComponent<ParticleSystem>());
         var zNetView = ___m_visualItem.GetComponent<ZNetView>();
-        if (zNetView)
-        {
+        if (zNetView) {
           // This is needed to stop placed item stop acting as a item drop in multiplayer.
           // Copypaste from ZNetScene::Destroy (but without actually destroying the object).
-          ZDO zdo = zNetView.GetZDO();
-          if (zdo != null)
-          {
+          var zdo = zNetView.GetZDO();
+          if (zdo != null) {
             zNetView.ResetZDO();
-            m_instances(ZNetScene.instance).Remove(zdo);
-            if (zdo.IsOwner()) ZDOMan.instance.DestroyZDO(zdo);
+            _ = Instances(ZNetScene.instance).Remove(zdo);
+            if (zdo.IsOwner()) {
+              ZDOMan.instance.DestroyZDO(zdo);
+            }
           }
-          UnityEngine.Object.Destroy(zNetView);
+          Object.Destroy(zNetView);
         }
       }
     }
