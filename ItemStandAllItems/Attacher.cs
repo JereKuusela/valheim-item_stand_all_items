e2b9e52a29b327e47ad2439438436ac542575b33
Jere Kuusela <jere.kuusela@iki.fi>
2022-04-01 16:51:08 +0300

    C#10 refactoring.

--- ItemStandAllItems/Attacher.cs
+++ ItemStandAllItems/Attacher.cs
@@ -1,82 +1,80 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+namespace ItemStandAllItems;
+public class Attacher {
+  public static bool Enabled(ItemStand obj) => obj && obj.m_name == "$piece_itemstand";
+  ///<summary>Legacy only finds the object with a collider. May not contain all models of the item resulting only in a partial item (like Graydward eye will miss the eye).</summary>
+  private static GameObject GetAttachObjectLegacy(GameObject item) {
+    var collider = item.transform.GetComponentInChildren<Collider>();
+    return collider ? collider.transform.gameObject : null;
+  }
 
-namespace ItemStandAllItems {
-  public static class Attacher {
-    public static bool Enabled(ItemStand obj) => obj && obj.m_name == "$piece_itemstand";
-    ///<summary>Legacy only finds the object with a collider. May not contain all models of the item resulting only in a partial item (like Graydward eye will miss the eye).</summary>
-    private static GameObject GetAttachObjectLegacy(GameObject item) {
-      var collider = item.transform.GetComponentInChildren<Collider>();
-      return collider ? collider.transform.gameObject : null;
+  ///<summary>Returns the only child (if possible).</summary>
+  private static GameObject GetChildModel(GameObject item) {
+    GameObject onlyChild = null;
+    foreach (Transform child in item.transform) {
+      if (child.gameObject.layer != item.layer) continue;
+      if (onlyChild) return null;
+      onlyChild = child.gameObject;
     }
-
-    ///<summary>Returns the only child (if possible).</summary>
-    private static GameObject GetChildModel(GameObject item) {
-      GameObject onlyChild = null;
-      foreach (Transform child in item.transform) {
-        if (child.gameObject.layer != item.layer) continue;
-        if (onlyChild) return null;
-        onlyChild = child.gameObject;
-      }
-      return onlyChild;
+    return onlyChild;
+  }
+  ///<summary>Finds a given transform. Copypaste from base game code.</summary>
+  private static GameObject GetTransform(GameObject item, string name) {
+    var transform = item.transform.Find(name);
+    return transform ? transform.gameObject : null;
+  }
+  public static GameObject GetAttach(GameObject item) {
+    // Base game also uses "attach" transform but explicitly disabled for some items.
+    // Check it first as it's the safest pick.
+    var obj = GetTransform(item, "attach");
+    if (obj) return obj;
+    if (Settings.UseLegacyAttaching) return GetAttachObjectLegacy(item);
+    // Child object is preferred as it won't contain ItemDrop script or weird transformation.
+    var childModel = GetChildModel(item);
+    if (childModel)
+      return childModel;
+    return item;
+  }
+  ///<summary>Hides the item stand if it has an item.</summary>
+  public static void HideIfItem(ItemStand obj) {
+    if (!Enabled(obj)) return;
+    if (!Settings.HideStandsWithItem) return;
+    var item = obj.m_visualItem;
+    var show = !obj.HaveAttachment() || !Settings.HideStandsWithItem;
+    // Layer check to filter the attached item.
+    var renderers = obj.GetComponentsInChildren<MeshRenderer>().Where(renderer => item == null || renderer.gameObject.layer == obj.gameObject.layer);
+    foreach (var renderer in renderers) {
+      if (renderer.enabled != show)
+        renderer.enabled = show;
     }
-    ///<summary>Finds a given transform. Copypaste from base game code.</summary>
-    private static GameObject GetTransform(GameObject item, string name) {
-      var transform = item.transform.Find(name);
-      return transform ? transform.gameObject : null;
+  }
+  ///<summary>Updates local transformation according to settings.</summary>
+  public static void UpdateItemTransform(ItemStand obj) {
+    if (!Attacher.Enabled(obj)) return;
+    if (obj.m_visualItem == null) return;
+    var transformations = Settings.CustomTransformations();
+    Settings.Offset(transformations, obj);
+    Settings.Rotate(transformations, obj);
+    Settings.Scale(transformations, obj);
+  }
+  ///<summary>Replaces ItemDrop script with an empty dummy object.</summary>
+  public static void ReplaceItemDrop(ItemStand obj) {
+    if (!Attacher.Enabled(obj)) return;
+    var item = obj.m_visualItem;
+    if (item == null || item.GetComponent<ItemDrop>() == null) return;
+    var attach = item.transform.parent;
+    var dummy = Object.Instantiate<GameObject>(new(), attach.position, attach.rotation, attach);
+    dummy.layer = item.layer;
+    List<GameObject> children = new();
+    foreach (Transform child in item.transform) {
+      if (child.gameObject.layer != dummy.layer) continue;
+      children.Add(child.gameObject);
     }
-    public static GameObject GetAttach(GameObject item) {
-      // Base game also uses "attach" transform but explicitly disabled for some items.
-      // Check it first as it's the safest pick.
-      var obj = GetTransform(item, "attach");
-      if (obj) return obj;
-      if (Settings.UseLegacyAttaching) return GetAttachObjectLegacy(item);
-      // Child object is preferred as it won't contain ItemDrop script or weird transformation.
-      var childModel = GetChildModel(item);
-      if (childModel)
-        return childModel;
-      return item;
-    }
-    ///<summary>Hides the item stand if it has an item.</summary>
-    public static void HideIfItem(ItemStand obj) {
-      if (!Enabled(obj)) return;
-      if (!Settings.HideStandsWithItem) return;
-      var item = obj.m_visualItem;
-      var show = !obj.HaveAttachment() || !Settings.HideStandsWithItem;
-      // Layer check to filter the attached item.
-      var renderers = obj.GetComponentsInChildren<MeshRenderer>().Where(renderer => item == null || renderer.gameObject.layer == obj.gameObject.layer);
-      foreach (var renderer in renderers) {
-        if (renderer.enabled != show)
-          renderer.enabled = show;
-      }
-    }
-    ///<summary>Updates local transformation according to settings.</summary>
-    public static void UpdateItemTransform(ItemStand obj) {
-      if (!Attacher.Enabled(obj)) return;
-      if (obj.m_visualItem == null) return;
-      var transformations = Settings.CustomTransformations();
-      Settings.Offset(transformations, obj);
-      Settings.Rotate(transformations, obj);
-      Settings.Scale(transformations, obj);
-    }
-    ///<summary>Replaces ItemDrop script with an empty dummy object.</summary>
-    public static void ReplaceItemDrop(ItemStand obj) {
-      if (!Attacher.Enabled(obj)) return;
-      var item = obj.m_visualItem;
-      if (item == null || item.GetComponent<ItemDrop>() == null) return;
-      var attach = item.transform.parent;
-      var dummy = Object.Instantiate<GameObject>(new GameObject(), attach.position, attach.rotation, attach);
-      dummy.layer = item.layer;
-      var children = new List<GameObject>();
-      foreach (Transform child in item.transform) {
-        if (child.gameObject.layer != dummy.layer) continue;
-        children.Add(child.gameObject);
-      }
-      foreach (GameObject child in children)
-        child.transform.SetParent(dummy.transform, false);
-      ZNetScene.instance.Destroy(item);
-      obj.m_visualItem = dummy;
-    }
+    foreach (GameObject child in children)
+      child.transform.SetParent(dummy.transform, false);
+    ZNetScene.instance.Destroy(item);
+    obj.m_visualItem = dummy;
   }
 }
--- ItemStandAllItems/ItemStandAllItems.cs
+++ ItemStandAllItems/ItemStandAllItems.cs
@@ -1,52 +1,50 @@
 ﻿using BepInEx;
 using HarmonyLib;
 using UnityEngine;
-
-namespace ItemStandAllItems {
-
-  [BepInPlugin("valheim.jere.item_stand_all_items", "ItemStandAllItems", "1.5.0.0")]
-  public class ItemStandAllItems : BaseUnityPlugin {
-    public void Awake() {
-      var harmony = new Harmony("valheim.jere.item_stand_all_items");
-      Settings.Init(Config);
-      harmony.PatchAll();
-    }
-  }
-
-  ///<summary>Adds additional check with the custom attach code.</summary>
-  [HarmonyPatch(typeof(ItemStand), "CanAttach")]
-  public class ItemStand_CanAttach {
-    public static void Postfix(ItemStand __instance, ItemDrop.ItemData item, ref bool __result) {
-      if (!Attacher.Enabled(__instance)) return;
-      if (__result) return;
-      __result = Attacher.GetAttach(item.m_dropPrefab) != null;
-    }
-  }
-
-  ///<summary>Replaces base game attach point finding with a custom one.</summary>
-  [HarmonyPatch(typeof(ItemStand), "GetAttachPrefab")]
-  public class ItemStand_GetAttachPrefab {
-    public static void Postfix(ItemStand __instance, GameObject item, ref GameObject __result) {
-      if (!Attacher.Enabled(__instance)) return;
-      if (__result == null) __result = Attacher.GetAttach(item);
-    }
-  }
-
-  ///<summary>Post processed the attached item.</summary>
-  [HarmonyPatch(typeof(ItemStand), "SetVisualItem")]
-  public class ItemStand_SetVisualItem {
-    public static void Postfix(ItemStand __instance) {
-      Attacher.ReplaceItemDrop(__instance);
-      Attacher.UpdateItemTransform(__instance);
-      Attacher.HideIfItem(__instance);
-    }
-  }
-
-  ///<summary>Instantly shows the item stand when removing the item.</summary>
-  [HarmonyPatch(typeof(ItemStand), "DropItem")]
-  public class ItemStand_DropItem {
-    public static void Postfix(ItemStand __instance) {
-      Attacher.HideIfItem(__instance);
-    }
+namespace ItemStandAllItems;
+[BepInPlugin("valheim.jere.item_stand_all_items", "ItemStandAllItems", "1.5.0.0")]
+public class ItemStandAllItems : BaseUnityPlugin {
+  public void Awake() {
+    Harmony harmony = new("valheim.jere.item_stand_all_items");
+    Settings.Init(Config);
+    harmony.PatchAll();
   }
 }
+
+///<summary>Adds additional check with the custom attach code.</summary>
+[HarmonyPatch(typeof(ItemStand), "CanAttach")]
+public class ItemStand_CanAttach {
+  static void Postfix(ItemStand __instance, ItemDrop.ItemData item, ref bool __result) {
+    if (!Attacher.Enabled(__instance)) return;
+    if (__result) return;
+    __result = Attacher.GetAttach(item.m_dropPrefab) != null;
+  }
+}
+
+///<summary>Replaces base game attach point finding with a custom one.</summary>
+[HarmonyPatch(typeof(ItemStand), "GetAttachPrefab")]
+public class ItemStand_GetAttachPrefab {
+  static void Postfix(ItemStand __instance, GameObject item, ref GameObject __result) {
+    if (!Attacher.Enabled(__instance)) return;
+    if (__result == null) __result = Attacher.GetAttach(item);
+  }
+}
+
+///<summary>Post processed the attached item.</summary>
+[HarmonyPatch(typeof(ItemStand), "SetVisualItem")]
+public class ItemStand_SetVisualItem {
+  static void Postfix(ItemStand __instance) {
+    Attacher.ReplaceItemDrop(__instance);
+    Attacher.UpdateItemTransform(__instance);
+    Attacher.HideIfItem(__instance);
+  }
+}
+
+///<summary>Instantly shows the item stand when removing the item.</summary>
+[HarmonyPatch(typeof(ItemStand), "DropItem")]
+public class ItemStand_DropItem {
+  static void Postfix(ItemStand __instance) {
+    Attacher.HideIfItem(__instance);
+  }
+}
+
